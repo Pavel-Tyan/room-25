aeac99877a3f6b0fd2637d0b1df93524cd55df96
Tyan_Pavel <ptyan_1@edu.hse.ru>
2024-02-27 20:23:03 +0300

    added translation for cardsInfo in cards.constants.ts

--- src/components/GameRules/GameRules.tsx
+++ src/components/GameRules/GameRules.tsx
@@ -3,23 +3,26 @@ import { Popup } from '../Popup/Popup';
 import { GameRulesProps } from './GameRules.props';
 import styles from './GameRules.module.css';
 import Image from 'next/image';
-import { cardsInfoRussian } from '@/constants/cards.constants';
+import { CardInfo, cardsInfoRussian } from '@/constants/cards.constants';
 import { cardsInfoEnglish } from '@/constants/cards.constants';
 import { RoomIcon } from '../RoomIcon/RoomIcon';
 
 export const GameRules = ({ language, isOpen, onClose }: GameRulesProps): JSX.Element => {
     let modalTitle: string;
+    let cardsInfo: CardInfo[];
 
     if (language === Languages.Russian) {
         modalTitle = 'ПРАВИЛА ИГРЫ';
+        cardsInfo = cardsInfoRussian;
     } else {
         modalTitle = 'GAME RULES';
+        cardsInfo = cardsInfoEnglish;
     }
 
     return (
         <Popup title={modalTitle} isOpen={isOpen} onClose={onClose}>
             <div className={styles.content}>
-                {cardsInfoRussian.map((cardInfo) => (
+                {cardsInfo.map((cardInfo) => (
                     <div key={cardInfo.alt} className={styles.card}>
                         <RoomIcon>
                             <Image
--- src/constants/cards.constants.ts
+++ src/constants/cards.constants.ts
@@ -1,4 +1,4 @@
-type CardInfo = {
+export type CardInfo = {
     alt: string;
     path: string;
     description: string;
@@ -20,7 +20,7 @@ export const cardsInfoRussian: CardInfo[] = [
     {
         alt: 'Иконка комнаты наблюдения',
         path: './rooms/binoculars.svg',
-        description: 'Комната наблюдения. Можно посмотреть любую закрытую комнату',
+        description: 'Комната наблюдения. Можно посмотреть любую закрытую комнату.',
     },
     {
         alt: 'Иконка затопленной комнаты',
@@ -96,90 +96,88 @@ export const cardsInfoRussian: CardInfo[] = [
 
 export const cardsInfoEnglish: CardInfo[] = [
     {
-        alt: 'Иконка центральной комнаты',
+        alt: 'Central room icon',
         path: './rooms/arrows.svg',
         description:
-            'Центральная комната. Это комната, в которой все заключённые начинают игру. Находясь здесь, вы можете выбрать только действия «Заглянуть» и «Передвинуться». Двигать (использовать действие «Контролировать») эту комнату запрещено.',
+            'Central room. This is the room where all prisoners start the game. While here, you can only select the “Peek” and “Move” actions. It is prohibited to move (use the “Control” action) this room.',
     },
     {
-        alt: 'Иконка комнаты с кислотной баней',
+        alt: 'Acid bath room icon',
         path: './rooms/acid.svg',
         description:
-            'Кислотная баня. Как только двое персонажей оказываются в этой комнате, тот из них, кто зашёл первым, умирает.',
+            'Acid bath. As soon as two characters are in this room, the one who entered first dies.',
     },
     {
-        alt: 'Иконка комнаты наблюдения',
+        alt: 'Surveillance room icon',
         path: './rooms/binoculars.svg',
-        description: 'Комната наблюдения. Можно посмотреть любую закрытую комнату',
+        description: 'Observation room. You can view any closed room.',
     },
     {
-        alt: 'Иконка затопленной комнаты',
+        alt: 'Flooded room icon',
         path: './rooms/droplet.svg',
         description:
-            'Затопленная комната. Как только персонаж заходит в эту комнату, она закрывается изнутри. Никто не может войти в неё до конца игры. Если после второго действия следующего раунда персонаж из неё не вышел, он умирает.',
+            'Flooded room. As soon as a character enters this room, it is locked from the inside. No one can enter it until the end of the game. If after the second action of the next round the character does not exit, he dies.',
     },
     {
-        alt: 'Иконка темной комнаты',
+        alt: 'Dark room icon',
         path: './rooms/eye.svg',
-        description:
-            'Темная комната. Находясь в этой комнате, вы не можете совершать действие «Заглянуть».',
+        description: 'A dark room. While in this room, you cannot take the Peek action.',
     },
     {
-        alt: 'Иконка комнаты контроля',
+        alt: 'Control room icon',
         path: './rooms/gear.svg',
         description:
-            'Комната контроля. Передвиньте любой ряд комнат (кроме среднего) в любом направлении по вашему выбору (как при совершении действия «Контролировать»).',
+            'Control room. Move any row of rooms (except the middle one) in any direction of your choice (as when taking the Control action).',
     },
     {
-        alt: 'Иконка комнаты с тюрьмой',
+        alt: 'Prison room icon',
         path: './rooms/prison.svg',
         description:
-            'Тюрьма. Выбраться из комнаты можно только к другому персонажу в соседней комнате или в Центральную комнату, если она находится по соседству.',
+            'Jail. You can only get out of the room to another character in the next room or to the Central Room, if it is located next door.',
     },
     {
-        alt: 'Иконка комнаты смерти',
+        alt: 'Death room icon',
         path: './rooms/skull.svg',
-        description:
-            'Комната смерти. Оказавшись в этой комнате, ваш персонаж мгновенно умирает.',
+        description: 'Death room. Once in this room, your character dies instantly.',
     },
     {
-        alt: 'Иконка пустой комнаты',
+        alt: 'Empty room icon',
         path: './rooms/smile.svg',
-        description: 'Пустая комната. Обычная комната без дополнительных эффектов.',
+        description: 'Empty room. An ordinary room without additional effects.',
     },
     {
-        alt: 'Иконка комнаты с морозилкой',
+        alt: 'Freezer room icon',
         path: './rooms/snowflake.svg',
         description:
-            'Морозилка. Находясь в этой комнате, можно запрограммировать только одно действие во время фазы программирования.',
+            'Freezer. While in this room, you can only program one action during the programming phase.',
     },
     {
-        alt: 'Иконка комнаты с водоворотом',
+        alt: 'Whirlpool room icon',
         path: './rooms/tornado.svg',
-        description: 'Водоворот. Перемещает персонажа в центральную комнату.',
+        description: 'Whirlpool. Moves the character to the central room.',
     },
     {
-        alt: 'Иконка комнаты с ловушкой',
+        alt: 'Trap room icon',
         path: './rooms/trap.svg',
         description:
-            'Ловушка. Следующим действием нужно покинуть комнату, иначе вас убьют.',
+            'Trap. The next action is to leave the room, otherwise you will be killed.',
     },
     {
-        alt: 'Иконка комнаты иллюзий',
+        alt: 'Illusion room icon',
         path: './rooms/trick.svg',
         description:
-            'Комната иллюзий. Персонаж перемещается в любую закрытую комнату на выбор и сразу применяет ее эффект. Перемещённая комната Иллюзий остаётся открытой до конца игры.',
+            'Room of illusions. The character moves to any closed room of his choice and immediately applies its effect. The moved Illusion room remains open until the end of the game.',
     },
     {
-        alt: 'Иконка комнаты-близняшки',
+        alt: 'Twin room icon',
         path: './rooms/twins.svg',
         description:
-            '⎯ Комната-Близняшка. Если вторая комната-близняшка уже открыта, то персонаж перемещается в неё. Если нет, ничего не происходит.',
+            'Twin Room. If the second twin room is already open, then the character moves into it. If not, nothing happens.',
     },
     {
-        alt: 'Иконка комнаты 25',
+        alt: 'Room 25 icon',
         path: './rooms/25.svg',
         description:
-            'Комната 25. Это комната выхода. Когда в ней собираются все заключённые, один из них должен передвинуть её к выходу, используя действие контролировать.',
+            'Room 25. This is the exit room. When all the prisoners are gathered in it, one of them must move it to the exit using the control action.',
     },
-]; // поменяй на англ
+];
